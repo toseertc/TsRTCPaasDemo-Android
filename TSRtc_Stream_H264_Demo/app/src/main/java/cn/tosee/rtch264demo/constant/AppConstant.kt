5dc0c9fec92cd4ea5dd7d8471a2e557e660926b9
gaoerhan <erhan.gao@tosee.cn>
2022-04-02 10:31:11 +0800

    feat:修改appid，修改包名，修改测试环境地址

--- TSRtc_Stream_H264_Demo/app/src/main/java/cn/tosee/rtch264demo/constant/AppConstant.kt
+++ TSRtc_Stream_H264_Demo/app/src/main/java/cn/tosee/rtch264demo/constant/AppConstant.kt
@@ -2,9 +2,10 @@ package cn.tosee.rtch264demo.constant
 
 object AppConstant {
     //填写对应相关的appid
-    const val DEFAULT_APPID="Drv4Q0KB14Y85qma"
+    const val DEFAULT_APPID = "2020112714291613322098613675048981332168688175226882"
+    const val RELEASE_APPID="Drv4Q0KB14Y85qma"
 
-    const val devEnv="{\"api\":\"https://api.rzrtc.com\",\"log\":\"https://data-center-dev.duobeiyun.com\"}"
+    const val devEnv="{\"api\":\"https://rtc-api-dev.tosee.cn\",\"log\":\"https://data-center-dev.tosee.cn\"}"
     const val CHANNEL_ID = "channelId"
     const val USER_ID = "userId"
 
